fa3b81d78b6b3efa091620b284e78466e1791e38
refu04 <refu28013@gmail.com>
2022-03-22 07:20:41 +0900

    Playerの移動を実装

--- Assets/Scripts/Player/PlayerCore.cs
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UniRx;
 
 public class PlayerCore : MonoBehaviour
 {
     //HP
+    [SerializeField]
     private int _hp;
 
     public int HP
@@ -13,13 +17,51 @@ public class PlayerCore : MonoBehaviour
         set { _hp = value; }
     }
 
-    
+    //移動スピード
+    [SerializeField]
+    private float _moveSpeed;
+
+    public float MoveSpeed
+    {
+        get { return _moveSpeed; }
+        set { _moveSpeed = value; }
+    }
+
+    //入力イベント
+    private InputEventProvider _inputEventProvider;
 
     async void Start()
     {
-        while(true)
+        _inputEventProvider = GetComponent<InputEventProvider>();
+
+        // CancellationTokenSourceを生成
+        var cts = new CancellationTokenSource();
+
+        while (true)
         {
-            
+            await Move(cts);
         }
+    }
+
+    async UniTask Move(CancellationTokenSource cts)
+    {
+        //FixedUpdateに切り替える
+        await UniTask.Yield(PlayerLoopTiming.FixedUpdate, cts.Token);
+
+        //移動処理
+        _inputEventProvider.MoveDirection
+            //初期値は無視する
+            .Skip(1)
+            //移動
+            .Subscribe(x => transform.position += x * _moveSpeed);
+
+        //移動量が０で無ければ
+        if(_inputEventProvider.MoveDirection.Value != Vector3.zero)
+        {
+            //移動するのを待つ
+            await UniTask.WaitUntilValueChanged(transform, x => x.position);
+        }
+        
+
     }
 }
